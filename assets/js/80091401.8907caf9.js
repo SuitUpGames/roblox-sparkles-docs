5729ecd2a65da05cc04ad622b50cc56f194bca50
Joshua DeBoer <joshuadeboer707@gmail.com>
2024-02-02 18:49:54 -0500

    Update docs 2-2-24

---
+++ assets/js/80091401.8907caf9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1812],{2048:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":":SetTutorialFinished","id":"SetTutorialFinished","level":3}]')}}]);
